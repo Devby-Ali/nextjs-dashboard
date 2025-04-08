b7beca24cf5bf3456289e9b5d53b5ca0c5a46a20
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-09 00:00:03 +0330

    Setting fonts and images with next.js principles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import "@/app/ui/global.css";
+import { exo_2 } from "@/app/ui/fonts"
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${exo_2.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Exo_2, IBM_Plex_Mono } from "next/font/google";
+
+export const exo_2 = Exo_2({ subsets: ["latin"] });
+
+export const ibmPlexMono = IBM_Plex_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
