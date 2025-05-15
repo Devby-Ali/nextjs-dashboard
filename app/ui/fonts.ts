48c1cbf491d1239f644f0a803a511a38f33397b1
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-15 19:13:39 +0330

    Add primary & secoundary fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+import { Exo_2 } from "next/font/google";
+
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const exo_2 = Exo_2({ subsets: ['latin'], weight: ["400", "700"] })
