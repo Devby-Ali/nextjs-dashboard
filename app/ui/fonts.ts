2a9ce9a0363f4f71d5e57f7bcc93a9d6e9d3ae83
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-15 22:46:12 +0330

    discard change font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,9 @@
 import { Inter } from "next/font/google";
-import { Exo_2 } from "next/font/google";
-
+import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const exo_2 = Exo_2({ subsets: ['latin'], weight: ["400", "700"] })
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
